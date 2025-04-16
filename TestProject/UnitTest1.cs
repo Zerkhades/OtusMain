85ab09730300edb22da5e42b676fb63414fea968
Zerkhades <zerkhade@gmail.com>
2025-04-17 00:11:31 +0300

    Добавьте файлы проекта.

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace TestProject
+{
+    public class UnitTest1
+    {
+        //publiv double[] solve(double a, double b, double c) { }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
